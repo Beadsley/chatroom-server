003adb3d3bf6c8e5fe14a00d10f42d8b7fec3ff6
Salt-Admin <salt-admin@Salt-Admins-MacBook-Pro.local>
2020-06-13 14:04:41 +0200

    Further heroku configuration

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,13 @@ import http from 'http';
 import cors from 'cors';
 import { handleNewUser, handleMessage, handleDisconnect, handleTermination } from './controllers/controller.chatroom';
 
+const PORT = process.env.PORT || 5000;
+
 const app = express();
+app.use(cors());
+app.set('port', PORT);
 const server = http.createServer(app);
 const io = socketio(server);
-const PORT = process.env.PORT || 8080;
-app.use(cors());
 
 io.on('connection', (socket: SocketIO.Socket) => {
   socket.on('new-user', handleNewUser(socket));
@@ -24,4 +26,8 @@ process.on('SIGTERM', handleTermination(io, server));
 
 process.on('SIGINT', handleTermination(io, server));
 
-server.listen(PORT);
+// server.listen(PORT);
+
+server.listen(PORT, () => {
+  console.log('Listening to port ' + PORT);
+});
