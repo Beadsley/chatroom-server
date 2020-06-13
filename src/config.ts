c69275b009e450995584754db89e75ff56faa018
Salt-Admin <salt-admin@Salt-Admins-MacBook-Pro.local>
2020-06-13 11:29:40 +0200

    USER_EXISTS_MESSAGE changed'

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@
 export enum constants {
   // error constants
   LOG_IN_ERROR = 'LOG_IN',
-  USER_EXISTS_MESSAGE = 'user already exists',
+  USER_EXISTS_MESSAGE = 'Nickname already taken',
 
   INACTIVITY_LIMIT = 600000,
   // INACTIVITY_LIMIT = 10000,
